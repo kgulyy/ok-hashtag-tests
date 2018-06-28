085a9c97a06665c08674ac67b48a3fa509702800
Konstantin Gulyy <kgulyy@my.com>
2018-06-29 00:28:12 +0300

    Add create method with default page name

--- src/main/java/com/my/kgulyy/qa/ok/pageobjects/PublicPage.java
+++ src/main/java/com/my/kgulyy/qa/ok/pageobjects/PublicPage.java
@@ -5,12 +5,18 @@ import com.my.kgulyy.qa.ok.components.PopupForCreateGroup;
 import org.openqa.selenium.WebDriver;
 
 public class PublicPage {
+    private static final String DEFAULT_PAGE_NAME = "My Page";
+
     private final WebDriver driver;
     private final GroupPage groupPage;
 
     public PublicPage(WebDriver driver) {
         this.driver = driver;
         groupPage = new GroupPage(driver);
+    }
+
+    public void create() {
+        create(DEFAULT_PAGE_NAME);
     }
 
     public void create(String name) {
