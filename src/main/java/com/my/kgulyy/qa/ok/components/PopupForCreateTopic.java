3b774d8444b405ce2b195fa7b2aa8b8069900822
Konstantin Gulyy <kgulyy@my.com>
2018-07-09 11:34:25 +0300

    Add wait for input topic text

--- src/main/java/com/my/kgulyy/qa/ok/components/PopupForCreateTopic.java
+++ src/main/java/com/my/kgulyy/qa/ok/components/PopupForCreateTopic.java
@@ -3,11 +3,17 @@ package com.my.kgulyy.qa.ok.components;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class PopupForCreateTopic {
     private static final String CREATE_TOPIC_FIELD_XPATH = "//a[contains(@data-l, \"OpenPostingPopup\")]";
     private static final String TOPIC_TEXT_FIELD_XPATH = "//div[@data-module=\"postingForm/mediaText\"]";
+    private static final String TOPIC_TEXT_FIELD_BLOCK_TEMPLATE_XPATH = "//div[@data-module=\"postingForm/mediaText\"]/div[text()=\"%s\"]";
     private static final String SUBMIT_BUTTON_XPATH = "//div[@class=\"posting_submit button-pro\"]";
+
+    private static final long TIMEOUT_IN_SECONDS = 10;
+    private static final long SLEEP_IN_MILLISECONDS = 100;
 
     private final WebDriver driver;
 
@@ -23,6 +29,13 @@ public class PopupForCreateTopic {
     public void setText(String text) {
         final WebElement textField = driver.findElement(By.xpath(TOPIC_TEXT_FIELD_XPATH));
         textField.sendKeys(text);
+        waitingForTextInput(text);
+    }
+
+    private void waitingForTextInput(String text) {
+        final String textFieldBlockXPath = String.format(TOPIC_TEXT_FIELD_BLOCK_TEMPLATE_XPATH, text);
+        final WebDriverWait wait = new WebDriverWait(driver, TIMEOUT_IN_SECONDS, SLEEP_IN_MILLISECONDS);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(textFieldBlockXPath)));
     }
 
     public void submit() {
